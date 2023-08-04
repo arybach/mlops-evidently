55ef7f8ac820ea48fcc9244869340c6eeb0b4455
Alex R <rybachek155@gmail.com>
2023-08-04 19:39:21 +0800

    turned off default ssl mode

--- src/utils/db_utils.py
+++ src/utils/db_utils.py
@@ -8,14 +8,14 @@ def open_sqa_session(arg: Union[str, sqlalchemy.engine.Engine]) -> sqlalchemy.or
     if isinstance(arg, str):
         # If the argument is a string, treat it as the database URI
         engine_args = {}
-        ssl_cert_path = os.getenv("SSL_CERT_PATH")
-        ssl_key_path = os.getenv("SSL_KEY_PATH")
-        if ssl_cert_path is not None and ssl_key_path is not None:
-            engine_args["connect_args"] = {
-                "sslmode": "require",
-                "sslcert": ssl_cert_path,
-                "sslkey": ssl_key_path,
-            }
+        # ssl_cert_path = os.getenv("SSL_CERT_PATH")
+        # ssl_key_path = os.getenv("SSL_KEY_PATH")
+        # if ssl_cert_path is not None and ssl_key_path is not None:
+        #     engine_args["connect_args"] = {
+        #         "sslmode": "require",
+        #         "sslcert": ssl_cert_path,
+        #         "sslkey": ssl_key_path,
+        #     }
         engine = sqlalchemy.create_engine(arg, **engine_args)
     else:
         # If the argument is already an SQLAlchemy Engine, use it directly
